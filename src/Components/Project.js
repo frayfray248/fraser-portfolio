17bdbba381951b592e15c1aa5d19148d4dc22cd5
frayfray248 <nicman248@hotmail.com>
2023-01-29 16:45:20 -0800

    No more anchor within an anchor in projects

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,7 +1,6 @@
 import Col from "react-bootstrap/Col"
 import styled from 'styled-components'
-import { ReactComponent as LinkIcon } from '../res/link-45deg.svg'
-import { ReactComponent as GitHubIcon } from '../res/github.svg'
+import { StyledGitHubIcon, StyledLinkIcon } from './PortfolioIcons'
 
 const StyledImage = styled.img`
 
@@ -22,29 +21,7 @@ const StyledCol = styled(Col)`
     }
 `
 
-const StyledLinkIcon = styled(LinkIcon)`
-width : 2rem;
-height : 2rem;
-color : ${props => props.theme.headerColor};
-margin : 4px;
-transition: all 0.5s;
-&:hover {
-    color: ${props => props.theme.color2};
-}
-`
-
-const StyledGitHubIcon = styled(GitHubIcon)`
-width : 2rem;
-height : 2rem;
-color : ${props => props.theme.headerColor};
-margin : 4px;
-transition: all 0.5s;
-&:hover {
-    color: ${props => props.theme.color2};
-}
-`
-
-const StyledAnchor = styled.a`
+const StyledImageAnchor = styled.a`
 &:link {
     text-decoration: inherit;
     color: inherit;
@@ -55,17 +32,25 @@ const StyledAnchor = styled.a`
 }
 `
 
+const IconAnchor = styled.a`
+
+>:hover {
+    color: ${props => props.theme.color2};
+  }
+`
+
 const Project = ({ title, description, img, projectUrl, repoUrl, mainLink }) => (
     <StyledCol className="m-2 p-2">
-        <StyledAnchor href={mainLink} target="_blank">
+        <StyledImageAnchor href={mainLink} target="_blank">
             <StyledImage className="mb-2" src={img}></StyledImage>
-            <h2>{title}</h2>
-            <p>{description}</p>
-            <footer className="align-bottom">
-                {projectUrl ? <a href={projectUrl} target="_blank"><StyledLinkIcon /></a> : ""}
-                {repoUrl ? <a href={repoUrl} target="_blank"><StyledGitHubIcon /></a> : ""}
-            </footer>
-        </StyledAnchor>
+        </StyledImageAnchor>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <footer className="align-bottom">
+            {projectUrl ? <IconAnchor href={projectUrl} target="_blank" rel="noreferrer"><StyledLinkIcon hover="true" /></IconAnchor> : ""}
+            {repoUrl ? <IconAnchor href={repoUrl} target="_blank" rel="noreferrer"><StyledGitHubIcon hover="true" /></IconAnchor> : ""}
+        </footer>
+
     </StyledCol>
 )
 
